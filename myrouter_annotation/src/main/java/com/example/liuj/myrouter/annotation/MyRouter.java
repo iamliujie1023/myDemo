6781b14da787ce59296816de3360b64a3d1f4e58
liujie <jie1.liu@husor.com>
2017-12-07 20:14:57 +0800

    0

--- myrouter_annotation/src/main/java/com/example/liuj/myrouter/annotation/MyRouter.java
+++ myrouter_annotation/src/main/java/com/example/liuj/myrouter/annotation/MyRouter.java
@@ -11,5 +11,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MyRouter {
-    String[] target();
+    public String[] target();
 }
--- myrouter_complie/src/main/java/com/example/liuj/myrouter/complie/MyRouterProcessor.java
+++ myrouter_complie/src/main/java/com/example/liuj/myrouter/complie/MyRouterProcessor.java
@@ -1,19 +1,19 @@
 package com.example.liuj.myrouter.complie;
 
 import com.example.liuj.myrouter.annotation.MyRouter;
-import com.example.liuj.utils.fileutil.MyFileRead;
-import com.example.liuj.utils.fileutil.MyFileWrite;
 import com.google.auto.service.AutoService;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
 
 /**
  * Created by liuj on 2017/12/6.
@@ -22,6 +22,16 @@ import javax.lang.model.element.TypeElement;
  */
 @AutoService(Processor.class)//该标记表明可以处理注解的能力
 public class MyRouterProcessor extends AbstractProcessor {
+
+    private Messager mMessager; //日志相关的辅助类
+
+    private void print(String msg) {
+        if (null == mMessager) {
+            return;
+        }
+        mMessager.printMessage(Diagnostic.Kind.NOTE, String.format("***** MyRouterProcessor %s ", msg));
+    }
+
 
     private synchronized void write(String status) {
 //        String raw = MyFileRead.getStringFromFile("fileProcessor", "log");
@@ -36,49 +46,53 @@ public class MyRouterProcessor extends AbstractProcessor {
     @Override
     public synchronized void init(ProcessingEnvironment processingEnvironment) {
         super.init(processingEnvironment);
-        LogUtil.println(" init");
-        write("init");
+        mMessager = processingEnvironment.getMessager();
+
+        String msg = "hahahaha";
+        mMessager.printMessage(Diagnostic.Kind.NOTE, "***** MyRouterProcessor " + msg);
+        mMessager.printMessage(Diagnostic.Kind.NOTE, String.format("***** MyRouterProcessor %s ", msg));
+        mMessager.printMessage(Diagnostic.Kind.ERROR, String.format("***** MyRouterProcessor %s ", msg));
+        mMessager.printMessage(Diagnostic.Kind.MANDATORY_WARNING, String.format("***** MyRouterProcessor %s ", msg));
+        mMessager.printMessage(Diagnostic.Kind.WARNING, String.format("***** MyRouterProcessor %s ", msg));
+        mMessager.printMessage(Diagnostic.Kind.OTHER, String.format("***** MyRouterProcessor %s ", msg));
+
+
+        print("init");
     }
 
     @Override
     protected synchronized boolean isInitialized() {
-        LogUtil.println(" isInitialized");
-        write("isInitialized");
+        print("isInitialized");
 
         return super.isInitialized();
     }
 
     /**
      * 包含本处理器想要处理的注解类型的合法全称。换句话说，你在这里定义你的注解处理器注册到哪些注解上。
+     *
      * @return
      */
     @Override
     public Set<String> getSupportedAnnotationTypes() {
-        LogUtil.println(" getSupportedAnnotationTypes");
-        LogUtil.println(" getSupportedAnnotationTypes --> MyRouter.class.getCanonicalName = " + MyRouter.class.getCanonicalName());
-
-        write("getSupportedAnnotationTypes --> MyRouter.class.getCanonicalName = " + MyRouter.class.getCanonicalName());
-
         Set<String> types = new LinkedHashSet<>();
         types.add(MyRouter.class.getCanonicalName());
+        print("getSupportedAnnotationTypes");
 
-        return super.getSupportedAnnotationTypes();
+        return types;
     }
 
     @Override
     public SourceVersion getSupportedSourceVersion() {
-        //     * 支持的JDK最新版本号
-        LogUtil.println(" getSupportedSourceVersion");
+        print("getSupportedSourceVersion");
 
-        write("getSupportedSourceVersion");
         return SourceVersion.latestSupported();
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-        LogUtil.println(" process");
-        write("process");
-        return false;
+        print("process");
+
+        return true;
     }
 
 
