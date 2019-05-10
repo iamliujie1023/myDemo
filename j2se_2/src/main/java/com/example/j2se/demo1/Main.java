2ce57263ad3f3cf523bfcf74dea5a42061d57439
liuj <546372069@qq.com>
2019-05-10 15:51:26 +0800

    111

--- j2se_2/src/main/java/com/example/j2se/demo1/Main.java
+++ j2se_2/src/main/java/com/example/j2se/demo1/Main.java
@@ -18,6 +18,8 @@ public class Main {
 
             System.out.println("update3");
 
+            System.out.println("update4");
+
 
 //            System.out.println("在另一个工程下反序列化");
 //            SerializeableDemoModel model = deserialize();
