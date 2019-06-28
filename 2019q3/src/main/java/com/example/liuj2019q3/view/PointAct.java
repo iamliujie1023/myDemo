c800228bd460ac08ef24cdb048fe7de0e3fe8ec7
liuj <546372069@qq.com>
2019-06-28 17:43:34 +0800

    1

--- 2019q3/src/main/java/com/example/liuj2019q3/view/PointAct.java
+++ 2019q3/src/main/java/com/example/liuj2019q3/view/PointAct.java
@@ -2,8 +2,10 @@ package com.example.liuj2019q3.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.example.liuj.sdk.base.BaseActivity;
+import com.example.liuj2019q3.R;
 
 /**
  * 坐标系相关
@@ -15,6 +17,9 @@ public class PointAct extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.q3_act_point);
+
+
     }
 
 
